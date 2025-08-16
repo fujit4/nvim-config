f079151c6aec957abd6bc57832869d8e7aea30b5
fujit4 <fujitash4@gmail.com>
2025-08-16 22:15:43 +0900

    補完にあわせてダブルクォーテーションをシングルクォーテーションに

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 -- 事前設定
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 vim.o.number = true
-vim.o.syntax = "on"
-vim.o.signcolumn = "yes"
-vim.o.encoding = "utf-8"
-vim.o.fileencoding = "utf-8"
-vim.o.fileencodings = "utf-8,sjis,euc-jp"
+vim.o.syntax = 'on'
+vim.o.signcolumn = 'yes'
+vim.o.encoding = 'utf-8'
+vim.o.fileencoding = 'utf-8'
+vim.o.fileencodings = 'utf-8,sjis,euc-jp'
 vim.o.bomb = false
 vim.o.tabstop = 4       -- 画面上のTab幅（見た目）
 vim.o.shiftwidth = 4    -- 自動インデント時の幅
@@ -14,11 +14,11 @@ vim.o.ambiwidth = 'single'
 
 
 -- 環境変数から `XDG_CONFIG_HOME` を取得して `runtimepath` に lazy.nvim を追加
-local config_home = vim.env.XDG_CONFIG_HOME or vim.fn.stdpath("config")
-vim.opt.rtp:prepend(config_home .. "/nvim/lazy/lazy.nvim")
+local config_home = vim.env.XDG_CONFIG_HOME or vim.fn.stdpath('config')
+vim.opt.rtp:prepend(config_home .. '/nvim/lazy/lazy.nvim')
 
 -- -- lazy.nvim 読み込み
-require("lazy").setup("plugins")
+require('lazy').setup('plugins')
 
 -- ctrl v でpasteモードにして貼り付けてnopasteに戻す
 vim.keymap.set("i", "<C-v>", function()
