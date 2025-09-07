3a2797b7809fa92910563829fce3f370ffb7b5d5
fujit4 <fujitash4@gmail.com>
2025-09-07 11:09:25 +0900

    format

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,24 +1,33 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo", "Conform" },
+  cmd = { "ConformInfo", "Format" },
   opts = {
-    -- Define your formatters
+    -- 保存時は自動フォーマットしない
+    format_on_save = false,
+
+    -- Prettier を使うファイルタイプ
     formatters_by_ft = {
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
-      css = { "prettier" },
-      html = { "prettier" },
-      json = { "prettier" },
-      yaml = { "prettier" },
-      markdown = { "prettier" },
-    },
-    -- Enable format on save
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
+      javascript        = { "prettier" },
+      typescript        = { "prettier" },
+      javascriptreact   = { "prettier" },
+      typescriptreact   = { "prettier" },
+      css               = { "prettier" },
+      html              = { "prettier" },
+      json              = { "prettier" },
+      yaml              = { "prettier" },
     },
   },
+  config = function(_, opts)
+    -- conform.nvim をセットアップ
+    require("conform").setup(opts)
+
+    -- 手動フォーマット用コマンドを定義
+    vim.api.nvim_create_user_command("Format", function()
+      require("conform").format({
+        async = true,
+        lsp_fallback = true,
+      })
+    end, { desc = "Format current buffer with conform.nvim" })
+  end,
 }
+
