bd431d8a016c28915d9b64b5a20efff0ec26c890
fujit4 <fujitash4@gmail.com>
2025-11-21 00:56:32 +0900

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,45 @@
 -- lua/plugins/colorscheme.lua
+--
+return {
+	{
+		"sainnhe/gruvbox-material",
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_background = "light"
+			vim.g.gruvbox_material_foreground = "hard"
+			vim.g.gruvbox_material_enable_bold = 1
+			vim.g.gruvbox_material_ui_contrast = "high"
+			vim.g.gruvbox_material_disable_italic_comment = 1
+			vim.o.background = "light"
+			vim.cmd("colorscheme gruvbox-material")
+		end,
+	},
+}
+
+-- return {
+--   {
+--     "sainnhe/everforest",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       -- 薄め・柔らかい light に最適な設定
+--       vim.g.everforest_background = "soft"     -- soft / medium / hard
+--       vim.g.everforest_better_performance = 1  -- 余計な処理を省いて軽量化
+--       vim.g.everforest_enable_italic = 0       -- 必要なら 1 に
+--       vim.g.everforest_transparent_background = 0
+--
+--       -- light モード
+--       vim.o.background = "light"
+--
+--       vim.cmd("colorscheme everforest")
+--     end,
+--   },
+-- }
+
+
+
+
+
 
 -- return {
 --   'projekt0n/github-nvim-theme',
@@ -15,24 +56,24 @@
 -- }
 --
 
-return {
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false,          -- 起動時に読み込む
-    priority = 1000,       -- colorscheme は最初に読み込む
-    config = function()
-      -- Nightfox の設定
-      require("nightfox").setup({
-        options = {
-          -- 必要ならここで透明化やスタイル設定もできる
-          -- transparent = true,
-        },
-      })
-
-      vim.cmd("colorscheme dawnfox")
-
-    end,
-  },
-}
+-- return {
+--   {
+--     "EdenEast/nightfox.nvim",
+--     lazy = false,          -- 起動時に読み込む
+--     priority = 1000,       -- colorscheme は最初に読み込む
+--     config = function()
+--       -- Nightfox の設定
+--       require("nightfox").setup({
+--         options = {
+--           -- 必要ならここで透明化やスタイル設定もできる
+--           -- transparent = true,
+--         },
+--       })
+--
+--       vim.cmd("colorscheme dawnfox")
+--
+--     end,
+--   },
+-- }
 
 
